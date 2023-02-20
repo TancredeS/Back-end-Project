c137b940f8324f7a7baeb0681f5f955db8e2b2e0
TancredeS <92266625+TancredeS@users.noreply.github.com>
2023-02-21 00:40:27 +0100

    Delete ClothesRefund directory
    test

--- ClothesRefund/App/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-# This comes from the Django documentation.
-class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'App'
