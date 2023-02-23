92edf729b2089f119bfc125a06ab135bed111dbe
TancredeS <92266625+TancredeS@users.noreply.github.com>
2023-02-23 19:17:25 +0100

    Add files via upload
    Final

--- ClothesRefund/App/models.py
+++ ClothesRefund/App/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # This is the model for the clothes, it contains the name, the material, the size, the brand and the color of the clothes.
 
 class Clothes(models.Model):
-    ClothesName = models.CharField(max_length=200 , default='Enter a name')
-    ClothesMaterial= models.CharField(max_length=200  ,default='Enter a material')
-    ClothesSize = models.CharField(max_length=200 ,default='Enter a size')
-    ClothesBrand = models.CharField(max_length=200 ,default='Enter a brand')
-    ClothesColor= models.CharField(max_length=200 ,default='Enter a color')
+    ClothesName = models.CharField(max_length=200 , default='ENTER A NAME')
+    ClothesMaterial= models.CharField(max_length=200  ,default='ENTER A MATERIAL')
+    ClothesSize = models.CharField(max_length=200 ,default='ENTER A SIZE')
+    ClothesBrand = models.CharField(max_length=200 ,default='ENTER A BRAND')
+    ClothesColor= models.CharField(max_length=200 ,default='ENTER A COLOR')
     IDclothes = models.AutoField(primary_key=True,)
     def __str__(self):
         return self.name
